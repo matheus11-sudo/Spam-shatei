c016a8a57d08a2b00de1f3de22a6ccdf9922180e
matheus11-sudo <77815396+matheus11-sudo@users.noreply.github.com>
2021-02-23 13:49:22 -0300

    Create spam-shatei.py

---
+++ spam-shatei.py
@@ -0,0 +1,26 @@
+import requests
+
+print("            SHATEI SPAM")
+print("            code: Shatei")      
+print("            creditos: Shatei")         
+print("  COLE O TOKEN E O ID CERTO SE N NAO IRA FUNCIONAR")
+print("  A FERRAMENTA N RECONHECE SE ESTA CERTO O TOKEN")
+token = str(input("\n  COPIE SEU TOKEN E COLE NO TERMINAL \n"))
+id = (input("\n  COLE O ID DO CANAL \n"))
+mensagem = input("\nQUAL SERA A MENSAGEM? \n")
+
+def spam():
+	payload = {
+		'content': mensagem
+	}
+	
+	header = {'authorization': token
+	}
+	
+	r =	requests.post("https://discord.com/api/v8/channels/" + id + "/messages", data=payload, headers=header)
+	
+	print("mensagem enviada")
+
+while True:
+	print(spam())
+	print(spam())
