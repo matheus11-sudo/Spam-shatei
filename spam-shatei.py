19364765e08f1df0f57bc66c45591d57c91a28aa
matheus11-sudo <77815396+matheus11-sudo@users.noreply.github.com>
2021-02-23 21:02:05 -0300

    Update spam-shatei.py

--- spam-shatei.py
+++ spam-shatei.py
@@ -1,17 +1,4 @@
 import requests
-import os
-import time
-import platform
-from colorama import Fore
-import sys
-import random
-
-platform = platform.system()
-menu = sys.argv[1::]
-if platform == 'Windows':
-	os.system('cls')
-else:
-	os.system('clear')
 
 print(" ____________________________________")
 print(" | ")
@@ -24,13 +11,25 @@ print(" |            ____________")
 print(" |            \033[41mCODE: Shatei\033[m ")
 print(" |")
 print(" |           _______________")
-print(" |           \033[36mCREDITOS: Shatei\033[m \n")       
-        
-print("  COLE O TOKEN E O ID CERTO SE N NAO IRA FUNCIONAR")
-print("  A FERRAMENTA N RECONHECE SE ESTA CERTO O TOKEN")
+print(" |           \033[36mCREDITOS: Shatei\033[m \n")         
+print("  \033[37mCOLE O TOKEN E O ID CERTO SE N NAO IRA FUNCIONAR\033[m")
+print("  \033[37m A FERRAMENTA N RECONHECE SE ESTA CERTO O TOKEN\033[m")
 token = str(input("\n  COPIE SEU TOKEN E COLE NO TERMINAL \n"))
 id = (input("\n  COLE O ID DO CANAL \n"))
 mensagem = input("\nQUAL SERA A MENSAGEM? \n")
 
 def spam():
-	payload
+	payload = {
+		'content': mensagem
+	}
+	
+	header = {'authorization': token
+	}
+	
+	r =	requests.post("https://discord.com/api/v8/channels/" + id + "/messages", data=payload, headers=header)
+	
+	print("mensagem enviada")
+
+while True:
+	print(spam())
+	print(spam())
